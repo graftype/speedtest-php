0ce12b228d5e3d6a7da45882b86d0800e5dfa830
Sergey Komlev <serjair@gmail.com>
2023-03-31 23:43:48 +0300

    round(): Passing null to parameter

--- src/Speedtest.php
+++ src/Speedtest.php
@@ -525,9 +525,9 @@ class Speedtest
      */
     public function share() {
         $results = $this->results();
-        $latency = (int)round($results->getLatency(), 0);
-        $download = (int)round($results->getDownload() / 1000, 0);
-        $upload = (int)round($results->getUpload() / 1000, 0);
+        $latency = !empty($results->getLatency()) ? (int)round($results->getLatency(), 0) : 0;
+        $download = !empty($results->getDownload()) ? (int)round($results->getDownload() / 1000, 0) : 0;
+        $upload = !empty($results->getUpload()) ? (int)round($results->getUpload() / 1000, 0) : 0;
         $bytesReceived = (int)$results->getBytesReceived();
         $bytesSent = (int)$results->getBytesSent();
         $hash = md5("$latency-$upload-$download-297aae72");
